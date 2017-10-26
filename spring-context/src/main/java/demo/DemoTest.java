56c67567cd9f35da7eb73a6e84ba9502bedda722
zhangdongjava <787395919@qq.com>
2017-10-26 16:53:01 +0800

    Initial commit

---
+++ spring-context/src/main/java/demo/DemoTest.java
@@ -0,0 +1,11 @@
+package demo;
+
+/**
+ * Created by Administrator on 2017/10/26.
+ */
+public class DemoTest {
+
+    public void print(){
+        System.out.println("DemoTest");
+    }
+}
---
+++ spring-context/src/test/java/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+import demo.DemoTest;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ * Created by Administrator on 2017/10/26.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        FileSystemXmlApplicationContext context;
+        context = new FileSystemXmlApplicationContext("classpath:applicationContext.xml");
+        context.getBean(DemoTest.class).print();
+    }
+}
